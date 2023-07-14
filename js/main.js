9505b00d50e1281d699eb8125a5692610a5112e5
cmk666 <12171475+cmk666@user.noreply.gitee.com>
2023-07-14 15:13:54 +0800

    Site updated: 2023-07-14 15:13:54

--- js/main.js
+++ js/main.js
@@ -1,21 +1,21 @@
-var is_dark_mode = "light"
+var is_dark_mode = "light";
 function dark_mode() {
   let list = ["--header-col", "--header-bgcol", "--card-bgcol",
     "--innercode-bgcol", "--code-bgcol", "--font-col",
-    "--title-col", "--border-col", "--background-img", "--dark-mode-bgcol"]
+    "--title-col", "--border-col", "--background-img", "--dark-mode-bgcol"];
   list.forEach((e) => {
-    let a = getComputedStyle(document.documentElement).getPropertyValue(e)
-    let b = getComputedStyle(document.documentElement).getPropertyValue(e + "-rev")
-    document.documentElement.style.setProperty(e, b)
-    document.documentElement.style.setProperty(e + "-rev", a)
-  })
-  is_dark_mode = (is_dark_mode == "light" ? "dark" : "light")
-  localStorage.setItem("dark-mode", is_dark_mode)
+    let a = getComputedStyle(document.documentElement).getPropertyValue(e);
+    let b = getComputedStyle(document.documentElement).getPropertyValue(e + "-rev");
+    document.documentElement.style.setProperty(e, b);
+    document.documentElement.style.setProperty(e + "-rev", a);
+  });
+  is_dark_mode = (is_dark_mode == "light" ? "dark" : "light");
+  localStorage.setItem("dark-mode", is_dark_mode);
 }
 
-let x = localStorage.getItem("dark-mode")
-if (!x) x = "light", localStorage.setItem("dark-mode", x)
-if (x == "dark") dark_mode()
+let x = localStorage.getItem("dark-mode");
+if (!x) x = "light", localStorage.setItem("dark-mode", x);
+if (x == "dark") dark_mode();
 
 $(document)
 .ready(function() {
